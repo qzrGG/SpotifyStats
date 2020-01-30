1188fea29966a246cc91ba277fdfceb841903545
qzrGG <quazarxd@gmail.com>
2020-01-30 23:09:23 +0000

    Deploying to gh-pages from  - f6139db49b3c1bbfa7fcb5c2e6f2218abac8c419 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9b5ae32e2134009ff51e977d77f25386.js"
+  "/SpotifyStats/precache-manifest.7a33a0fe566b4f0dc02381222f730847.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/SpotifyStats/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
