8eccb9fa02d2a033d6a57f9cf14135bbf3a5b617
piotr.lazarczyk <pelazaarczyk@gmail.com>
2020-01-31 12:12:16 +0100

    using hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Layout } from './components/Layout';
 import { Route } from 'react-router';
 import { Home } from './components/Home';
 import { Stats } from './components/Stats';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import About from './components/About';
 
 const App: React.FC = () => {
   const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href') as string;
 
   return (
-    <BrowserRouter basename={baseUrl}>
+    <HashRouter basename={baseUrl}>
       <Layout>
         <Route exact path='/' component={Home} />
         <Route path='/stats' component={Stats} />
         <Route path='/about' component={About} />
       </Layout>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
