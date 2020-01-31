a23260d5fea570f9c50c956bee94ce35675c49d8
qzrGG <quazarxd@gmail.com>
2020-01-31 00:27:48 +0000

    Deploying to gh-pages from  - 9febb8869dd1f51c64a68d6e5eca304493d6b1dd 🚀

---
+++ precache-manifest.247fd80776afb549967cedb77122ec05.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37d239cdfa8a12c1690c3780b3656003",
+    "url": "/SpotifyStats/index.html"
+  },
+  {
+    "revision": "8c35e7c2a9e8b2aee3f2",
+    "url": "/SpotifyStats/static/css/2.34cb2874.chunk.css"
+  },
+  {
+    "revision": "19587aefa69477e8fe65",
+    "url": "/SpotifyStats/static/css/main.db2739a0.chunk.css"
+  },
+  {
+    "revision": "8c35e7c2a9e8b2aee3f2",
+    "url": "/SpotifyStats/static/js/2.dfb0ebf2.chunk.js"
+  },
+  {
+    "revision": "9c3d41e256b36a69e384e90ce62f2327",
+    "url": "/SpotifyStats/static/js/2.dfb0ebf2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "19587aefa69477e8fe65",
+    "url": "/SpotifyStats/static/js/main.0f147d4d.chunk.js"
+  },
+  {
+    "revision": "ce81bff47b5f7053d187",
+    "url": "/SpotifyStats/static/js/runtime-main.d07385dc.js"
+  }
+]);
