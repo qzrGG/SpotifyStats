9febb8869dd1f51c64a68d6e5eca304493d6b1dd
piotr.lazarczyk <pelazaarczyk@gmail.com>
2020-01-31 01:25:14 +0100

    added instructions

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { NavLink } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 export class Home extends Component {
   static displayName = Home.name;
@@ -6,7 +8,14 @@ export class Home extends Component {
   render () {
     return (
       <div>
-        <h1>Spotify Stats</h1>
+        <h1>How to use it</h1>
+        <ol className="instructions">
+          <li>Go to <a href="https://www.spotify.com/en/account/privacy/" target="_blank">Spotify privacy settings</a> and scrlil down</li>
+          <li>Request a copy of your data</li>
+          <li>Wait a couple of days</li>
+          <li>Check your inbox and download the data</li>
+          <li>Unzip and upload your <i>StreamingHistory#.json</i> files <a href="/stats">HERE</a></li>
+        </ol>
       </div>
     );
   }
